a96f6acd5cf185df9890ba034f1844bea79f78f0
Kohl <kohltastrophe@gmail.com>
2025-05-23 09:13:16 -0500

    chore: Remove print

--- src/Icon/Attribute.lua
+++ src/Icon/Attribute.lua
@@ -7,8 +7,5 @@
 local RunService = game:GetService("RunService")
 local GroupService = game:GetService("GroupService")
 local Players = game:GetService("Players")
-if not RunService:IsStudio() then
-	print(`🍍 Running TopbarPlus {require(script.Parent.VERSION)} by ForeverHD`)
-end
 
 return {}
