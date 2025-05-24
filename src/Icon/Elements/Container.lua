aa0d4169ca78f5fe2923f9f75c8f89f810baa9a3
Kohl <kohltastrophe@gmail.com>
2025-05-23 22:39:02 -0500

    chore: Update insets

--- src/Icon/Elements/Container.lua
+++ src/Icon/Elements/Container.lua
@@ -1,13 +1,10 @@
 return function(Icon)
-	
 	local GuiService = game:GetService("GuiService")
-	local isOldTopbar = Icon.isOldTopbar
 	local container = {}
-	local guiInset = GuiService:GetGuiInset()
 	local isConsoleScreen = GuiService:IsTenFootInterface()
-	local startInset = if isOldTopbar then 12 else guiInset.Y - (44 + 2)
+	local startInset = 7
 	if isConsoleScreen then
-		startInset = 10
+		startInset = 7
 	end
 	local screenGui = Instance.new("ScreenGui")
 	screenGui:SetAttribute("StartInset", startInset)
@@ -24,10 +21,10 @@ return function(Icon)
 	end)
 
 	local holders = Instance.new("Frame")
-	local yDownOffset = if isOldTopbar then 2 else 0
+	local yDownOffset = 0
 	local ySizeOffset = -2
 	if isConsoleScreen then
-		yDownOffset += 13
+		yDownOffset = 1
 		ySizeOffset = 50
 	end
 	holders.Name = "Holders"
@@ -37,12 +34,11 @@ return function(Icon)
 	holders.Visible = true
 	holders.ZIndex = 1
 	holders.Parent = screenGui
-	
+
 	local screenGuiCenter = screenGui:Clone()
 	local holdersCenter = screenGuiCenter.Holders
-	local GuiService = game:GetService("GuiService")
 	local function updateCenteredHoldersHeight()
-		holdersCenter.Size = UDim2.new(1, 0, 0, GuiService.TopbarInset.Height+ySizeOffset)
+		holdersCenter.Size = UDim2.new(1, 0, 0, GuiService.TopbarInset.Height + ySizeOffset)
 	end
 	screenGuiCenter.Name = "TopbarCentered"
 	screenGuiCenter.ScreenInsets = Enum.ScreenInsets.None
@@ -52,37 +48,23 @@ return function(Icon)
 	container[screenGuiCenter.Name] = screenGuiCenter
 	GuiService:GetPropertyChangedSignal("TopbarInset"):Connect(updateCenteredHoldersHeight)
 	updateCenteredHoldersHeight()
-	
+
 	local screenGuiClipped = screenGui:Clone()
-	screenGuiClipped.Name = screenGuiClipped.Name.."Clipped"
+	screenGuiClipped.Name = screenGuiClipped.Name .. "Clipped"
 	screenGuiClipped.DisplayOrder += 1
 	Icon.baseDisplayOrderChanged:Connect(function()
 		screenGuiClipped.DisplayOrder = Icon.baseDisplayOrder + 1
 	end)
 	container[screenGuiClipped.Name] = screenGuiClipped
-	
+
 	local screenGuiCenterClipped = screenGuiCenter:Clone()
-	screenGuiCenterClipped.Name = screenGuiCenterClipped.Name.."Clipped"
+	screenGuiCenterClipped.Name = screenGuiCenterClipped.Name .. "Clipped"
 	screenGuiCenterClipped.DisplayOrder += 1
 	Icon.baseDisplayOrderChanged:Connect(function()
 		screenGuiCenterClipped.DisplayOrder = Icon.baseDisplayOrder + 1
 	end)
 	container[screenGuiCenterClipped.Name] = screenGuiCenterClipped
-	
-	if isOldTopbar then
-		task.defer(function()
-			local function decideToHideTopbar()
-				if GuiService.MenuIsOpen then
-					Icon.setTopbarEnabled(false, true)
-				else
-					Icon.setTopbarEnabled()
-				end
-			end
-			GuiService:GetPropertyChangedSignal("MenuIsOpen"):Connect(decideToHideTopbar)
-			decideToHideTopbar()
-		end)
-	end
-	
+
 	local holderReduction = -24
 	local left = Instance.new("ScrollingFrame")
 	left:SetAttribute("IsAHolder", true)
@@ -101,10 +83,10 @@ return function(Icon)
 	left.ScrollBarThickness = 0
 	left.BorderSizePixel = 0
 	left.Selectable = false
-	left.ScrollingEnabled = false--true
+	left.ScrollingEnabled = false --true
 	left.ElasticBehavior = Enum.ElasticBehavior.Never
 	left.Parent = holders
-	
+
 	local UIListLayout = Instance.new("UIListLayout")
 	UIListLayout.Padding = UDim.new(0, startInset)
 	UIListLayout.FillDirection = Enum.FillDirection.Horizontal
@@ -112,13 +94,13 @@ return function(Icon)
 	UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
 	UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Left
 	UIListLayout.Parent = left
-	
+
 	local center = left:Clone()
 	center.ScrollingEnabled = false
 	center.UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 	center.Name = "Center"
 	center.Parent = holdersCenter
-	
+
 	local right = left:Clone()
 	right.UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
 	right.Name = "Right"
